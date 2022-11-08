db00022b2e1c4fe6cf30805ae3356a28ad27f551
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-11-08 11:58:52 -0600

    completed syntax, types, and variables ex

---
+++ src/HelloWorld.java
@@ -0,0 +1,37 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello, World!");
+        int myFavoriteNumber = 21;
+        String myString = "I can't wait to make a game!";
+        System.out.print(myString);
+        double myNumber = 3.14;
+        System.out.print(myNumber);
+//        long myNumber = ;
+
+//        int x = 5;
+//        System.out.println(x++);
+//        System.out.println(x);
+//
+//        int x = 5;
+//        System.out.println(++x);
+//        System.out.println(x);
+
+//        String theNumberThree = "three";
+//        Object o = theNumberThree;
+//        int three = (int) o;
+
+//        int three = (int) "three";
+
+//        int x = 4;
+//        x = x + 5;
+//        System.out.println(x);
+
+        int x = 3;
+        int y = 4;
+        y = y * x;
+        System.out.println(x);
+        System.out.println(y);
+
+
+    }
+}
