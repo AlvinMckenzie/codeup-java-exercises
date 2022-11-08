19cd081a9313eb6dac3c3822bd32cd43ff9b43c5
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-11-08 14:49:24 -0600

    part two complete

--- src/ConsoleExercises.java
+++ src/ConsoleExercises.java
@@ -1,8 +1,24 @@
+import java.util.Scanner;
+
 public class ConsoleExercises {
     public static void main(String[] args) {
         double pi = 3.14159;
         System.out.println("The value of pi is approximately 3.14");
         System.out.printf("The value of pi is approximately %.2f%n", pi);
+
+
+//        Scanner scannerIO = new Scanner(System.in);
+//        System.out.print("How older were you when you bought your first car: ");
+//        int age = Integer.parseInt(scannerIO.next());
+//
+//        Scanner scannerStr = new Scanner(System.in);
+//        System.out.print("Enter three words: ");
+//        String str = scannerStr.nextLine();
+//
+//        System.out.println("You entered:-->\""+ age + "\"<--");
+//        System.out.println("You entered:-->\""+ str + "\"<--");
+
+
 
 
     }
