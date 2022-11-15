c39125fc4eca7bda478bc4c5e674d9c71afe65e5
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-11-15 17:45:03 -0600

    Circle util complete

--- src/shapes/Circle.java
+++ src/shapes/Circle.java
@@ -1,4 +1,14 @@
 package shapes;
 
 public class Circle {
+    private double raduis;
+
+    public double getArea(){
+        return Math.Pi * (math.pow(raduis,2));
+    }
+
+    public double getCircumference(){
+        return 2 * Math.Pi * raduis;
+    }
+
 }
