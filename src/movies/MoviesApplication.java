2d9df1290ccfc15f0b6c8c2fd0481c1ce1257f70
Alvin Mckenzie <mckenzie.a.alvin@gmail.com>
2022-11-15 20:31:41 -0600

    moviesApp function start

---
+++ src/movies/MoviesApplication.java
@@ -0,0 +1,31 @@
+package movies;
+
+
+import util.Input;
+
+import java.util.Scanner;
+
+public class MoviesApplication {
+    public static void main(String[] args) {
+        movieCat();
+    }
+
+    public static void movieCat(){
+        Input x = new Input(new Scanner(System.in));
+        int y = x.getInt();
+        System.out.println("What would you like to do?");
+        System.out.println("0 - exit");
+        System.out.println("1 - view all movies");
+        System.out.println("2 - view movies in the animated category");
+        System.out.println("3 - view movies in the drama category");
+        System.out.println("4 - view movies in the horror category");
+        System.out.println("5 - view movies in the scifi category");
+        System.out.println("6 - view movies in the musical category");
+        System.out.println("7 - view movies in the comedy category");
+
+
+        for (int i = 0; i < MoviesArray.findAll().length; i++){
+            if (y == 3 && MoviesArray.findAll()[i].getCategory() == "drama") {  System.out.println(MoviesArray.findAll()[i].getName());};
+        }
+    }
+}
